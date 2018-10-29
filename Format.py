38311d9a6f34a8937f9c22e8493feb60cb233f47
SatyaGraha5 <sailoer123@gmail.com>
2018-10-29 10:01:01 +0900

    General Format

---
+++ Format.py
@@ -0,0 +1,54 @@
+from collections import namedtuple
+
+
+class TESTCASE():
+    def __init__(self,):
+        
+
+    def solve(self):
+
+
+def data_preprocessing(input_file):
+    testcases=['index_start_from_1']
+    with open(input_file,'r') as f:
+        num_of_testcase=int(f.readline())
+        for n_th_testcase in range(1,num_of_testcase+1):
+            #----------------------Parsing Logic----------------------#
+            for char in f.readline().rstrip('\n').split(" "))
+            testcases.append(TESTCASE())
+            #--------------------------End----------------------------#
+    return testcases
+
+
+def model(testcases):
+    answers = ['index_start_from_1']
+    for testcase in testcases[1:]:
+        answer = testcase.solve()
+        answers.append(answer)
+    return answers
+
+
+def view(output_file,answers):    
+    fmt_string="Case #{0}: {1}\n"
+    with open(output_file,'w') as f:
+        for n_th, answer in enumerate(answers[1:],1):
+            f.write(fmt_string.format(n_th,answer))  
+
+
+def controller():
+    #Test Conditions
+    suspect = "large-practice"
+    difficulty = 'D'
+    input_file = '{}-{}.in'.format(difficulty,suspect)
+    output_file = '{}-{}.out'.format(difficulty,suspect)
+    #----------------------Data Preprocessing----------------------#
+    testcases = data_preprocessing(input_file)
+    #----------------------------Model-----------------------------#
+    answers = model(testcases = testcases)
+    #-----------------------------View-----------------------------#
+    view(output_file = output_file,answers = answers)   
+
+
+if __name__=="__main__":
+    controller()
+        
