f1cfea5647f04979fa2cf616b75e4dcb083b69d9
SatyaGraha5 <sailoer123@gmail.com>
2018-11-20 21:08:30 +0900

    Debugging

--- 2016/Close_Match/Close_Match.py
+++ 2016/Close_Match/Close_Match.py
@@ -14,25 +14,150 @@ choose the way that minimizes the Coders' score.
 If there is more than one way to attain the minimum absolute difference 
 while also minimizing the Coders' score, choose the way that minimizes the Jammers' score.
 '''
-from collections import namedtuple
+from collections import namedtuple,defaultdict
 
-
-class TESTCASE():
+class CODER_JAMMER_PAIR():
     def __init__(self,coder,jammer):
         self.C = coder
         self.J = jammer
 
+    def __getattr__(self, name):
+        if name == 'C':
+            return self.C
+        elif name == 'J':
+            return self.J
+        else:
+            raise IndexError
 
-    def solve(self):
+    def __str__(self):
+        return "Coder: {} Jammer: {}".format(self.C.decode('utf-8'),self.J.decode('utf-8'))
 
-
-class NUMBER_STRING_WITH_Q_MARK():
-    def __init__(self,original_string):
-        self.S = original_string
-        self.G = [] #Guesses
     
 
-    def 
+
+class TESTCASE():
+    def __init__(self,coder,jammer):
+        self.C = bytearray(coder,'utf-8')
+        self.J = bytearray(jammer,'utf-8')
+
+
+    def solve(self):
+        '''
+            initial setting
+        '''
+        max_depth = len(self.C)
+        possible_case_tree = defaultdict(list)
+        initial_pair = CODER_JAMMER_PAIR(self.C, self.J)
+        possible_case_tree[0].append(initial_pair)
+        #possible_case_tree[0].append(self.current_decisions(initial_pair,0))
+        for current_decision in self.current_decisions(initial_pair,0):
+            possible_case_tree[0].append(current_decision)
+        current_depth = 1
+
+        '''
+            making decision tree
+        '''
+        while current_depth < max_depth:
+            #print(current_depth)
+            past_depth = current_depth - 1
+            for current_pair in possible_case_tree[past_depth]:
+                for current_decision in self.current_decisions(current_pair,current_depth):
+                    possible_case_tree[current_depth].append(current_decision)
+            current_depth += 1
+        #print("possible_case_tree: {}".format(possible_case_tree))
+        self.pretty_print_defaultdict(possible_case_tree)
+
+
+    def current_decisions(self,current_pair,depth):
+        '''
+            return list of CODER_JAMMER_PAIR
+        '''
+        C = current_pair.C
+        J = current_pair.J
+        c = chr(C[depth])
+        j = chr(J[depth])
+        if is_integer(c) is True and is_integer(j) is True:
+            print("{} is integer and {} is integer".format(c,j))
+            if are_same(c,j) is True:
+                return [CODER_JAMMER_PAIR(C,J)]
+            elif are_same(c,j) is False:
+                return [self.fill_question_mark(C,J,depth)]
+        elif is_integer(c) is True and is_integer(j) is False:
+            print("{} is integer and {} is not integer".format(c,j))
+            bigger = J
+            bigger[depth] = (c + 1)%10
+            same = J
+            smaller = J
+            smaller[depth] = (c - 1)%10
+            return [CODER_JAMMER_PAIR(C,bigger), CODER_JAMMER_PAIR(C,same), CODER_JAMMER_PAIR(C,smaller)]
+        elif is_integer(c) is False and is_integer(j) is True:
+            print("{} is not integer and {} is integer".format(c,j))
+            bigger = C
+            bigger[depth] = (j + 1)%10
+            same = C
+            smaller = C
+            smaller[depth] = (j - 1)%10
+            return [CODER_JAMMER_PAIR(bigger,J), CODER_JAMMER_PAIR(same,J), CODER_JAMMER_PAIR(smaller,J)]
+        elif is_integer(c) is False and is_integer(j) is False:
+            print("{} is not integer and {} is not integer".format(c,j))
+            one_C = C
+            one_C[depth] = 1
+            zero_C = C
+            zero_C[depth] = 0
+            one_J = J
+            one_J[depth] = 1
+            zero_J = J
+            zero_J[depth] = 0
+            return [CODER_JAMMER_PAIR(one_C,zero_J), CODER_JAMMER_PAIR(zero_C,zero_J), CODER_JAMMER_PAIR(zero_C,one_C)]
+    
+
+    def fill_question_mark(self,string_a,string_b,depth):
+        '''
+            return list
+        '''
+        if string_a[depth] > string_b[depth]:
+            return CODER_JAMMER_PAIR(self.fill_smallest(string_a), self.fill_biggest(string_b))
+        elif string_a[depth] < string_b[depth]:
+            return CODER_JAMMER_PAIR(self.fill_biggest(string_a),self.fill_smallest(string_b))
+
+
+    def fill_smallest(self,bigger):
+        '''
+            return str
+        '''
+        return bigger.replace(b'?',b'0')
+
+
+    def fill_biggest(self,smaller):
+        '''
+            return str
+        '''
+        return smaller.replace(b'?',b'9')
+
+
+    def pretty_print_defaultdict(self,possible_case_tree):
+        for depth, decisions in possible_case_tree.items():
+            print("depth: {}".format(depth))
+            for decision in decisions:
+                print(decision)
+
+
+
+def is_integer(suspect):
+    try:
+        int(suspect)
+    except ValueError:
+        return False
+    else:
+        return True 
+
+
+def are_same(a,b):
+    if a == b:
+        return True
+    else:
+        return False
+
 
 
 def data_preprocessing(input_file):
--- 2016/Close_Match/test.py
+++ 2016/Close_Match/test.py
@@ -1,33 +1,60 @@
-def func():
-    for i in range(5):
-        if i == 2:
-            continue
-        print(i)
+from collections import defaultdict
+def test_defaultdict():
+    test_dict = defaultdict(list)
+    print(test_dict[0])
+
+
+def test_is_digit(test_char):
+    print(test_char.isdigit())
+
+
+def test_is_int(test_char):
+    try:
+        int(test_char)
+    except ValueError:
+        return False
+    else:
+        return True
+
+
+def test_list_append():
+    test_list = []
+    a = '1'
+    b = '?'
+    test_list.append([a,b])
+    print(test_list)
+
+def test_string_index():
+    test_string = "1?3"
+    print(test_string[1])
+
+
+def test_replace():
+    test_string = "1?3"
+    print(test_string.replace('?','0'))
+
+
+def test_same():
+    a = '1'
+    b = '1'
+    print(a == b)
+
+
+def test_string_int():
+    a = list("123")
+    b = "5"
+    a[0] = str(int(b) + 1)
+    print(a)
+
+
+def test_pretty_print_dict():
+    possible_case_tree = defaultdict(list)
+    decision = bytearray('1??','utf-8')
+    possible_case_tree[0] = decision
+    #print(possible_case_tree[0].decode('utf-8'))
+    for index,current_decision in possible_case_tree.items():
+        print(current_decision)
+        #print(current_decision.decode('utf-8'))
 
 if __name__ == "__main__":
-    func()
-
-'''
-    def fill_biggest(self,suspect):
-        return suspect.replace('?','9')
-    
-    def fill_smallest(self,suspect):
-        return suspect.replace('?','0')
-
-    def first_different_point(self,bigger,smaller):
-        b = self.fill_biggest(bigger)
-        s = self.fill_smallest(smaller)
-        difference = int(b) - int(s)
-        return difference, b, s
-
-    def judge_minimum_difference(self,suspect_tuple):
-
-
-    def fill_max(self,string):
-        return 1
-    
-
-    def fill_min(self,string):
-        return 1
-        return suspect.replace('?','0')
-'''
+    test_pretty_print_dict()
